9b01dfd9580d46ade18c1e39577b965173996733
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2012-08-07 10:47:12 +0200

    License information added to new files.

--- association_database.adb
+++ association_database.adb
@@ -1,3 +1,12 @@
+--  The Beer-Ware License (revision 42)
+--
+--  Jacob Sparre Andersen <jacob@jacob-sparre.dk> wrote this. As long as you
+--  retain this notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a beer in
+--  return.
+--
+--  Jacob Sparre Andersen
+
 with
   Ada.Containers.Hashed_Maps,
   Ada.Exceptions,
