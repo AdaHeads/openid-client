3b46f0f04b0c4cffcdfc217532a18beb2aa20dd7
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2012-11-21 15:09:28 +0100

    You can't do it this way, but the interface is nice.  (AWS.OpenID.Manual_Dispatching would have to be made non-generic - at least with respect to Host_Name - for this to work.)

---
+++ aws-openid-dynamic_dispatching.adb
@@ -0,0 +1,25 @@
+--  The Beer-Ware License (revision 42)
+--
+--  Jacob Sparre Andersen <jacob@jacob-sparre.dk> wrote this. As long as you
+--  retain this notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a beer in
+--  return.
+--
+--  Jacob Sparre Andersen
+
+package body AWS.OpenID.Dynamic_Dispatching is
+   procedure Register
+     (Dispatcher : in out AWS.Services.Dispatchers.URI.Handler) is
+      use AWS.Services.Dispatchers.URI;
+   begin
+      Register (Dispatcher => Dispatcher,
+		URI        => Handlers.Log_In.URI,
+		Action     => Handlers.Log_In.Service'Access);
+      Register (Dispatcher => Dispatcher,
+		URI        => Handlers.Validate.URI,
+		Action     => Handlers.Validate.Service'Access);
+      Register (Dispatcher => Dispatcher,
+		URI        => Handlers.Log_Out.URI,
+		Action     => Handlers.Log_Out.Service'Access);
+   end Register;
+end AWS.OpenID.Dynamic_Dispatching;
---
+++ aws-openid-dynamic_dispatching.ads
@@ -0,0 +1,39 @@
+--  The Beer-Ware License (revision 42)
+--
+--  Jacob Sparre Andersen <jacob@jacob-sparre.dk> wrote this. As long as you
+--  retain this notice you can do whatever you want with this stuff. If we meet
+--  some day, and you think this stuff is worth it, you can buy me a beer in
+--  return.
+--
+--  Jacob Sparre Andersen
+
+with
+  AWS.OpenID.Manual_Dispatching,
+  AWS.Services.Dispatchers.URI,
+  AWS.Status;
+
+generic
+   Host_Name       : String;
+   Log_In_Page     : String := "login";
+   Return_To_Page  : String := "return_to";
+   Logged_In_Page  : String := "logged_in";
+   Logged_Out_Page : String := "logged_out";
+package AWS.OpenID.Dynamic_Dispatching is
+   package Handlers is
+     new AWS.OpenID.Manual_Dispatching (Host_Name       => Host_Name,
+					Log_In_Page     => Log_In_Page,
+					Return_To_Page  => Return_To_Page,
+					Logged_In_Page  => Logged_In_Page,
+					Logged_Out_Page => Logged_Out_Page);
+   
+   procedure Register
+     (Dispatcher : in out AWS.Services.Dispatchers.URI.Handler);
+
+   function Is_Authenticated (Request : in AWS.Status.Data) return Boolean
+     renames Handlers.Is_Authenticated;
+
+   Not_Authenticated : exception renames Handlers.Not_Authenticated;
+
+   function Authenticated_As (Request : in AWS.Status.Data) return String
+     renames Handlers.Authenticated_As;
+end AWS.OpenID.Dynamic_Dispatching;
